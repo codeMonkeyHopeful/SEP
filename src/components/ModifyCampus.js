76c1b061bd600b95ae2288fd02d2d36bc75bbb3f
Ryan Jasinski <jasinski.17@gmail.com>
2019-07-08 20:42:59 -0400

    Campus Creation Form Added

--- src/components/ModifyCampus.js
+++ src/components/ModifyCampus.js
@@ -3,16 +3,47 @@ import { getSingleCampus } from "../Reducer";
 import { connect } from "react-redux";
 import { HashRouter, Route, Link, Switch } from "react-router-dom";
 
-export const ModifyCampus = props => {
-  return <h1>1</h1>;
-};
+export class ModifyCampus extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      address: "",
+      description: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Address</label>
+        <input type="text" name="address" />
+        <label>Description</label>
+        <input type="text" name="description" />
+        <button type="submit">Create Campus</button>
+      </form>
+    );
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const newCampus = {
+      name: event.target.name.value,
+      address: event.target.address.value,
+      description: event.target.description
+    };
+    console.log(newCampus);
+    //createCampusReact(newCampus)
+  }
+}
 
 const mapStateToProps = state => {
-  return { campus: state.singleCampus }; //update
+  return { campus: state.campuses }; //update
 };
 
 const mapDispatchToProps = dispatch => ({
-  getSingleCampusReact: id => dispatch(getSingleCampus(id)) //update
+  createCampusReact: campus => dispatch(getSingleCampus(id)) //update
 });
 export default connect(
   mapStateToProps,
